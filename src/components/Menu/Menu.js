8718f7aeb95c0507385009f3242cb6a467958aad
r616on <r616on@gmail.com>
2021-12-05 23:48:16 +0300

    fix_1_0

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -24,12 +24,14 @@ function Menu() {
           Forza Karting
         </NavLink>
       </nav>
-      <Routes>
-        <Route path="/drift" element={<DriftPage />} />
-        <Route path="/timeattack" element={<TimeAttackPage />} />
-        <Route path="/forza" element={<ForzaPage />} />
-        <Route path="/" element={<HomePage />} />
-      </Routes>
+      <div className="menu">
+        <Routes>
+          <Route path="/drift" element={<DriftPage />} />
+          <Route path="/timeattack" element={<TimeAttackPage />} />
+          <Route path="/forza" element={<ForzaPage />} />
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </div>
     </div>
   );
 }
